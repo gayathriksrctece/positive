95d3dc60b9973d34819bcfe2d781000973023d04
gayathriksrctece <gayathri301485@gmail.com>
2017-06-09 17:23:41 +0530

    Create Negative.java

---
+++ Negative.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Negative
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n>0)
+{
+System.out.println("number is positive");
+}
+if(n<0)
+{
+System.out.println("number is negative");
+}
+if(n==0)
+{
+System.out.println("number is zero");
+}
+}
+}
